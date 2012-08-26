889f25ab54375e043e21abe52b78ec13602eab11
Jordi Olivares <jordi@jolivar.es>
2012-08-26 15:48:38 +0200

    Fixed the effed up sys.argv[] and added more comments

--- images.py
+++ images.py
@@ -4,16 +4,17 @@ import Image
 import sys, os
 
 def lastBits (n, count):
+    """ Returns COUNT bits from number N """
     return n % (2**count)
 
 def cleanBits (n, count):
+    """ Cleans the last COUNT bits from number N """
     return n ^ lastBits (n, count)
 
 def chopBits (n, x, y, z):
     """
-    Chops the number n into the x, y, and z bits (Total bits
-    must count to 8). Ex: number 243 (0b111.10.011) into 3, 2, 3
-    bits -> (7 (111), 2 (10), 3 (011))
+    Chops the number n into the x, y, and z bits (Total bits must count to 8).
+    Ex: number 243 (0b111.10.011) into 3, 2, 3 bits -> (7 (111), 2 (10), 3 (011))
     """
     first = (n >> (y + z)) % (2**x)
     middle = (n >> z) % (2**y)
@@ -23,8 +24,7 @@ def chopBits (n, x, y, z):
 def bitSum (bits, x, y, z):
     """
     Bits: tuple containig the hidden bits
-    x, y, z: the bits they were from the image (same format as
-    chopBits)
+    x, y, z: the bits they were from the image (same format as chopBits)
     """
     big = bits[0] << (y + z)
     med = bits[1] << z
@@ -32,6 +32,10 @@ def bitSum (bits, x, y, z):
     return big + med + lit
 
 def add (effeduptuple):
+    """
+    This is the ugliest hack I've written in my life. I don't even remember
+    what form the effeduptuple took. I think it was ((X, Y),(M, N))
+    """
     neater = list (zip (effeduptuple[0], effeduptuple[1]))
     return map (lambda n: n[0] + n[1], neater)
 
@@ -61,32 +65,34 @@ def decode (image, x, y, z):
              lastBits (I[1], y),
              lastBits (I[2], z) )
             for I in im ]
-    im = [ bitSum (N, 3, 2, 3) for N in im ]
+    im = [ bitSum (N, x, y, z) for N in im ]
     return im
 
 def main():
     pool = multiprocessing.Pool() # For the parallel map()
     if sys.argv[1] == "decode":
-        source = Image.open(sys.argv[-2])
+        source = Image.open(sys.argv[1])
         print ("Decoding the encoded...")
-        secret = decode (sys.argv[-2], 3, 2, 3)
+        secret = decode (sys.argv[1], 3, 2, 3)
         output = Image.new("L", source.size)
         output.putdata(secret)
-        output.save(sys.argv[-1])
+        output.save(sys.argv[2])
     elif sys.argv[1] == "encode":
-        im = Image.open(sys.argv[-3])
+        im = Image.open(sys.argv[1])
         print ("Chopping Bits...")
-        secret = hidden(sys.argv[-3])
+        secret = hidden(sys.argv[1])
         print ("Cooking the Pot...")
-        messenger = carrier(sys.argv[-2])
+        messenger = carrier(sys.argv[2])
         print ("Potting the Bits...")
         final = zip (secret, messenger)
+        # In the first versions the variables used a disproportionate amount of
+        # RAM
         del (secret)
         del (messenger)
         final = list (pool.map (add, final))
         final = list (pool.map (tuple, final))
         output = Image.new("RGB",im.size)
         output.putdata(final)
-        output.save(sys.argv[-1])
+        output.save(sys.argv[3])
 
 main()
